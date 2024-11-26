99ecad59bb06fb8f469731c6bf60d764cb0a357d
danybeltran <josuebeltran_2010@hotmail.com>
2024-11-25 21:26:56 -0600

    remove deleted components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
 import "./globals.css"
-import { FetchConfig } from "@/httpr/components"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
